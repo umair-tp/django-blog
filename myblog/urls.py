f151bbac61823ce55195a4ead5d60a432213f0f9
Mohammad Umair <umair@testpress.in>
2018-03-16 16:26:12 +0530

    Basic Blog and category features

--- myblog/urls.py
+++ myblog/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from blog import views
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^blog/view/(?P<slug>[^\.]+).html', views.view_post, name='view_blog_post'),
+    url(r'^blog/category/(?P<slug>[^\.]+).html', views.view_category, name='view_blog_category'),
 ]
